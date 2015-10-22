c13d2e470428814ee968f26b5c3e4bb06bbdafe2
jaxgeller <jacksongeller@gmail.com>
2015-10-22 16:04:14 -0400

    making progress on the animation

--- src/js/main.js
+++ src/js/main.js
@@ -4,32 +4,38 @@
 .split-details
 */
 
+const spacing = 90;
+let scrollBuffer = 0;
 
 window.addEventListener('scroll', () => {
   let rect = document.querySelector('.split-details').getBoundingClientRect();
-  let top = window.scrollY + rect.top;
-
   let el = document.querySelector('.split-description');
+  let scrollPos = window.scrollY;
 
-  // if (window.scrollY < top) {
-  //   el.style.position = "absolute";
-  //   el.style.top = "0";
-  //   el.style.bottom = "initial";
+  let buffer = 0;
+
+  if (scrollPos > scrollBuffer) {
+    buffer = -1 * 120;
+  }
+
+  scrollBuffer = scrollPos
+
+  // // if inside div
+  // if (scrollPos >= scrollPos + rect.top) {
+  //   el.style.position = 'fixed'
   // }
 
-  // if (window.scrollY > top) {
-  //   el.style.position = "fixed";
-  //   el.style.top = "0";
-  //   el.style.bottom = "90px";
+  // // on down rect.bottom - window.innerHeight < -120 &&
+  // if (rect.bottom - window.innerHeight < buffer) {
+  //   el.style.position = 'absolute'
+  //   el.style.bottom = '0'
+  //   el.style.top  = 'initial'
   // }
 
-  // if (rect.bottom - window.innerHeight < -90) {
-  //   el.style.position = "absolute";
-  //   el.style.bottom = "90px";
-  //   el.style.top = "initial";
+  // // going to top
+  // if (scrollPos < scrollPos + rect.top -el.getBoundingClientRect().top ) {
+  //   el.style.position = 'absolute'
+  //   el.style.bottom = 'initial'
+  //   el.style.top  = '0'
   // }
-
-
-  // bottom
-  // $0.getBoundingClientRect().bottom - window.innerHeight
 });
