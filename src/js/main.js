078af6b2baf52078a9ad256845bc118f2d191bdb
jaxgeller <jacksongeller@gmail.com>
2015-10-24 13:48:17 -0400

    fix up fixing a bit

--- src/js/main.js
+++ src/js/main.js
@@ -6,27 +6,56 @@
 
 const spacing = 90;
 let scrollBuffer = 0;
+let dir;
 
 window.addEventListener('scroll', () => {
-  let rect = document.querySelector('.split-details').getBoundingClientRect();
-  let el = document.querySelector('.split-description');
   let scrollPos = window.scrollY;
 
-  let buffer = 0;
-
-  if (scrollPos > scrollBuffer) {
-    buffer = -1 * 120;
-  }
+  scrollPos > scrollBuffer ?
+    dir = 'down' :
+    dir = 'up';
 
   scrollBuffer = scrollPos
+  let rect = document.querySelector('.split-details').getBoundingClientRect();
+  let el = document.querySelector('.split-description');
 
-  // // if inside div
-  // if (scrollPos >= scrollPos + rect.top) {
-  //   el.style.position = 'fixed'
-  // }
+  let offset = window.innerHeight - el.getBoundingClientRect().height
+
+
+  if (dir === 'down') {
+    if (scrollPos >= scrollPos + rect.top) {
+      el.style.position = 'fixed'
+    }
+
+    offset = window.innerHeight - el.getBoundingClientRect().bottom
+
+    if (window.innerHeight > rect.bottom + offset) {
+      el.style.position = 'absolute';
+      el.style.top = 'initial';
+      el.style.bottom = '0'
+    }
+  }
+
+  if (dir === 'up') {
+    if (window.innerHeight - rect.bottom < 0) {
+      el.style.position = 'fixed';
+      el.style.top = 'initial';
+      el.style.bottom = '0'
+    }
+
+    if (rect.top - offset > 0) {
+      el.style.position = 'absolute';
+      el.style.top = '0';
+      el.style.bottom = 'initial'
+    }
+  }
+
+
+  // difference = window.innerHeight - $0.getBoundingClientRect().height
+
 
   // // on down rect.bottom - window.innerHeight < -120 &&
-  // if (rect.bottom - window.innerHeight < buffer) {
+  // if (rect.bottom - window.innerHeight < -120) {
   //   el.style.position = 'absolute'
   //   el.style.bottom = '0'
   //   el.style.top  = 'initial'
