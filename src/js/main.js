29073a787f9e284c7bd1ad9544cbab40fdbaa4a6
jaxgeller <jacksongeller@gmail.com>
2015-10-24 16:51:44 -0400

    add correct animation

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,7 @@ window.addEventListener('scroll', () => {
   let offset = window.innerHeight - el.getBoundingClientRect().height
 
 
+
   if (dir === 'down') {
     if (scrollPos >= scrollPos + rect.top) {
       el.style.position = 'fixed'
@@ -37,34 +38,31 @@ window.addEventListener('scroll', () => {
   }
 
   if (dir === 'up') {
-    if (window.innerHeight - rect.bottom < 0) {
+    if (el.getBoundingClientRect().top >= 0) {
       el.style.position = 'fixed';
-      el.style.top = 'initial';
-      el.style.bottom = '0'
+      el.style.top = '0';
+      el.style.bottom = 'initial'
     }
 
-    if (rect.top - offset > 0) {
+    if (rect.top >= 0) {
       el.style.position = 'absolute';
       el.style.top = '0';
       el.style.bottom = 'initial'
     }
+
+    // if (window.innerHeight - rect.bottom < 0 ) {
+    //   el.style.position = 'fixed';
+    //   el.style.top = 'initial';
+    //   el.style.bottom = '0'
+    // }
+
+    // if (rect.top - offset > 0) {
+    //   el.style.position = 'absolute';
+    //   el.style.top = '0';
+    //   el.style.bottom = 'initial'
+    // }
   }
 
 
   // difference = window.innerHeight - $0.getBoundingClientRect().height
-
-
-  // // on down rect.bottom - window.innerHeight < -120 &&
-  // if (rect.bottom - window.innerHeight < -120) {
-  //   el.style.position = 'absolute'
-  //   el.style.bottom = '0'
-  //   el.style.top  = 'initial'
-  // }
-
-  // // going to top
-  // if (scrollPos < scrollPos + rect.top -el.getBoundingClientRect().top ) {
-  //   el.style.position = 'absolute'
-  //   el.style.bottom = 'initial'
-  //   el.style.top  = '0'
-  // }
 });
