43d17d7d0dba7e4d8ed7892285934a805cf004de
jaxgeller <jacksongeller@gmail.com>
2015-10-24 22:03:39 -0400

    finish window scroll

--- src/js/main.js
+++ src/js/main.js
@@ -7,62 +7,30 @@
 const spacing = 90;
 let scrollBuffer = 0;
 let dir;
+let els = [].slice.call(document.querySelectorAll('.split'));
 
 window.addEventListener('scroll', () => {
-  let scrollPos = window.scrollY;
+  els.forEach(item => {
+    let rect = item.getBoundingClientRect();
+    let top = rect.top + window.scrollY;
 
-  scrollPos > scrollBuffer ?
-    dir = 'down' :
-    dir = 'up';
-
-  scrollBuffer = scrollPos
-  let rect = document.querySelector('.split-details').getBoundingClientRect();
-  let el = document.querySelector('.split-description');
-
-  let offset = window.innerHeight - el.getBoundingClientRect().height
-
-
-
-  if (dir === 'down') {
-    if (scrollPos >= scrollPos + rect.top) {
-      el.style.position = 'fixed'
-    }
-
-    offset = window.innerHeight - el.getBoundingClientRect().bottom
-
-    if (window.innerHeight > rect.bottom + offset) {
-      el.style.position = 'absolute';
-      el.style.top = 'initial';
-      el.style.bottom = '0'
-    }
-  }
-
-  if (dir === 'up') {
-    if (el.getBoundingClientRect().top >= 0) {
+    let el = item.querySelector('.split-description');
+    let coordinator = item.querySelector('.split-details');
+    if (window.scrollY > top && window.scrollY < top + rect.height) {
       el.style.position = 'fixed';
-      el.style.top = '0';
+      el.style.top = 0;
+      el.style.bottom = 'initial';
+
+      if (coordinator.getBoundingClientRect().bottom - el.getBoundingClientRect().height <= 0) {
+        el.style.position = 'absolute';
+        el.style.bottom = 0;
+        el.style.top = 'initial';
+      }
+
+    } else {
+      el.style.position = 'absolute'
       el.style.bottom = 'initial'
-    }
-
-    if (rect.top >= 0) {
-      el.style.position = 'absolute';
       el.style.top = '0';
-      el.style.bottom = 'initial'
     }
-
-    // if (window.innerHeight - rect.bottom < 0 ) {
-    //   el.style.position = 'fixed';
-    //   el.style.top = 'initial';
-    //   el.style.bottom = '0'
-    // }
-
-    // if (rect.top - offset > 0) {
-    //   el.style.position = 'absolute';
-    //   el.style.top = '0';
-    //   el.style.bottom = 'initial'
-    // }
-  }
-
-
-  // difference = window.innerHeight - $0.getBoundingClientRect().height
+  });
 });
