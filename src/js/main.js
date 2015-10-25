1b57b42d1a9d7dd37d03f439004e5e320be30bcf
jaxgeller <jacksongeller@gmail.com>
2015-10-25 14:21:24 -0400

    add responsiveness

--- src/js/main.js
+++ src/js/main.js
@@ -51,31 +51,33 @@ export default class Jumper {
 let els = [].slice.call(document.querySelectorAll('.split'));
 
 window.addEventListener('scroll', () => {
-  els.forEach(item => {
-    let windowPos = window.scrollY;
-    let rect = item.getBoundingClientRect();
-    let top = rect.top + windowPos;
+  if (window.innerHeight > 860) {
+    els.forEach(item => {
+      let windowPos = window.scrollY;
+      let rect = item.getBoundingClientRect();
+      let top = rect.top + windowPos;
 
-    let el = item.querySelector('.split-description');
-    let coordinator = item.querySelector('.split-details');
+      let el = item.querySelector('.split-description');
+      let coordinator = item.querySelector('.split-details');
 
-    if (windowPos > top && windowPos < top + rect.height) {
-      el.style.position = 'fixed';
-      el.style.top = 0;
-      el.style.bottom = 'initial';
+      if (windowPos > top && windowPos < top + rect.height) {
+        el.style.position = 'fixed';
+        el.style.top = 0;
+        el.style.bottom = 'initial';
 
-      if (coordinator.getBoundingClientRect().bottom - el.getBoundingClientRect().height <= 0) {
-        el.style.position = 'absolute';
-        el.style.bottom = 0;
-        el.style.top = 'initial';
+        if (coordinator.getBoundingClientRect().bottom - el.getBoundingClientRect().height <= 0) {
+          el.style.position = 'absolute';
+          el.style.bottom = 0;
+          el.style.top = 'initial';
+        }
+
+      } else {
+        el.style.position = 'absolute'
+        el.style.bottom = 'initial'
+        el.style.top = '0';
       }
-
-    } else {
-      el.style.position = 'absolute'
-      el.style.bottom = 'initial'
-      el.style.top = '0';
-    }
-  });
+    });
+  }
 });
 
 
